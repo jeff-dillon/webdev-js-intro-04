cb7d92c75efcf694397c850fe57ae09febbdc573
jeff-dillon <dillonjg@gmail.com>
2025-01-08 13:39:56 -0600

    Completed Step 2

--- js/assignment.js
+++ js/assignment.js
@@ -3,16 +3,25 @@
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
+const responseElement = document.getElementById("response")
 
 // create a mutable variable called age and do not assign it a value.
+let age;
 
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
     // Write your code below
+    if(age >= 21) {
+        responseElement.textContent ='You can vote and purchase alchol.';
+    } else if(age >= 18) {
+        responseElement.textContent ='You can vote, but you cannot purchase alcohol.';
+    } else {
+        responseElement.textContent ='You cannot vote and you cannot purchase alcohol.';
+    }
 
-}
+};
 
 
 submissionBtn.addEventListener("click", function () {
